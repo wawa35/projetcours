3b9fe237d8dbeb6d9a94f88aadad7ee177a7c13b
walid bahij <walidbahij35@gmail.com>
2024-05-03 18:51:18 +0200

    site afec maj

---
+++ exo.js
@@ -0,0 +1,35 @@
+//let message = "bonjour le monde";
+//message = ("il pleut ajd");
+//console.log(message);
+//console.log("message");
+//const age = 17;
+//console.log(alert(age));
+//const name = "John ";
+//const foot ="plat";
+//const result = name +"a " + age + " ans et il a les pieds " + foot ;
+//console.log = (alert(result));
+
+
+// Objet est une paire clef valeur
+//let obj= {
+  // Peugeot : "francaise", 
+  // VW : "deutch", 
+  // lexus : "japon"};
+//let result = " Ma voiture est une ";
+//console.log(alert(result + obj.Peugeot));
+
+//tabl sert a créer un tableau
+let array = ["février","mars","avril"]
+// Push() fonction native de JS ajt des élements en fin du tableau
+array.push("mai","juin","juillet");
+console.log(array.length);
+// .pop() supprime le dernier élement
+ for ( let i = 0; i < array.length; i ++) {
+    console.log(array[i]);
+    //applique le js du html
+    let getvalue =`<h1>${array[i]}</h1>`
+document.getElementById(temoin)
+ }
+
+
+
